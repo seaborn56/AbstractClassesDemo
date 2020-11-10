597d16305558f915a67ed95ed0a68d485a3fdfb1
Cole Patterson <seaborncpatterson@gmail.com>
2020-11-09 19:38:17 -0600

    finished

---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ConsoleUI
+{
+    public class Motorcycle : Vehicle
+    {
+        public Motorcycle()
+        {
+        }
+
+        public bool HasSideCart { get; set; } = false;
+
+        public override void DriveAbstract()
+        {
+            Console.WriteLine($"This motorcycle is in drive!");
+        }
+
+        public override void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is a {GetType().BaseType.Name} this is a virtually in drive");
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -32,18 +32,35 @@ namespace ConsoleUI
             */
 
             // Create a list of Vehicle called vehicles
+            var vehicles = new List<Vehicle>();
 
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * - new it up as one of each derived class
              * Set the properties with object initializer syntax
              */
+            var focus = new Car() { HasTrunk = true, Make = "Ford", Model = "Focus", Year = 2013 };
+            var motorcycle = new Motorcycle() { HasSideCart = true, Make = "HD", Model = "Chopper", Year = 1978 };
+
+            Vehicle sedan = new Car() { Make = "Dodge", Model = "Blah", Year = 2019 };
+            Vehicle sport = new Car() { HasTrunk = false, Make = "Ferari", Model = "Something", Year = 2000 };
+
 
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate over each of the properties
              */
+            vehicles.Add(focus);
+            vehicles.Add(motorcycle);
+            vehicles.Add(sedan);
+            vehicles.Add(sport);
 
+            foreach(var veh in vehicles)
+            {
+                Console.WriteLine($"Make {veh.Make} Model {veh.Model} Year {veh.Year}");
+                veh.DriveAbstract();
+                Console.WriteLine("--------------------");
+            }
             // Call each of the drive methods for one car and one motorcycle
 
             #endregion            
